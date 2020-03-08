84ef139fe390611a19f811a72a4637e1556d9a07
rahulreddy1558 <rahulreddy1558@gmail.com>
2020-03-08 15:16:03 -0400

    Data Structures

--- src/main/java/com/group/respository/HibernateCustomer.java
+++
@@ -1,9 +0,0 @@
-package com.group.respository;
-
-import com.group.model.Customer;
-
-import java.util.List;
-
-public interface HibernateCustomer {
-    List<Customer> findall();
-}
--- src/main/java/com/group/service/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.group.service;
-
-import com.group.model.Customer;
-
-import java.util.List;
-
-public interface CustomerService {
-    List<Customer> findall();
-}
